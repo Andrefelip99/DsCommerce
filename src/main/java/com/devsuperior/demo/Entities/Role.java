c2d291508f2fc1977e2c66ff68b337f1c4984101
andre felipe <andre.felipe106@icloud.com>
2026-03-13 18:13:07 -0300

    Criacao do Role

---
+++ src/main/java/com/devsuperior/demo/Entities/Role.java
@@ -0,0 +1,50 @@
+package com.devsuperior.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_role")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((authority == null) ? 0 : authority.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (authority == null) {
+            if (other.authority != null)
+                return false;
+        } else if (!authority.equals(other.authority))
+            return false;
+        return true;
+    }
+
+}
